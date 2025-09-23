8b61d2ea2c20e18211e982e9941ca306c5da8816
Tristan <tharris17.j@gmail.com>
2025-09-23 16:07:00 -0500

    Adds Process Control Block oss maintains process control block for each child process oss prints process table every half second of simulated time adds auto shutdown after 60 seconds of real time

--- oss.cpp
+++ oss.cpp
@@ -6,9 +6,24 @@
 #include <cstdio>
 #include <cstdlib>
 #include <sys/wait.h>
+#include <vector>
+#include <iomanip>
+#include <signal.h>
 
 using namespace std;
 
+struct PCB {
+    bool occupied;
+    pid_t pid;
+    int start_sec;
+    int start_nano;
+};
+
+key_t sh_key;
+int shmid;
+int *shm_clock;
+int *sec;
+vector <PCB> table(10);
 const int increment_amount = 1000;
 
 void increment_clock(int* sec, int* nano, int amt) {
@@ -39,7 +54,7 @@ pid_t child_Terminated() {
     return -1;
 }
 
-void launch_worker(float time_limit) {
+pid_t launch_worker(float time_limit) {
     pid_t worker_pid = fork();
     if (worker_pid < 0) {
         cerr << "fork failed" << endl;
@@ -59,6 +74,64 @@ void launch_worker(float time_limit) {
         execv(args[0], args);
         cerr << "Exec failed" << endl;
         exit(1);
+    }
+    return worker_pid;
+}
+
+int find_empty_pcb(const vector<PCB> &table) {
+    for (size_t i = 0; i < table.size(); ++i) {
+        if (!table[i].occupied) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+int remove_pcb(vector<PCB> &table, pid_t pid) {
+    for (size_t i = 0; i < table.size(); ++i) {
+        if (table[i].occupied && table[i].pid == pid) {
+            table[i].occupied = false;
+            return i;
+        }
+    }
+    return -1;
+}
+
+void print_process_table(const std::vector<PCB> &table) {
+    using std::cout;
+    using std::endl;
+    cout << std::left
+         << std::setw(6)  << "Index"
+         << std::setw(10) << "Occ"
+         << std::setw(12) << "PID"
+         << std::setw(12) << "StartSec"
+         << std::setw(12) << "StartNano" << endl;
+    cout << std::string(52, '-') << endl;
+
+    for (size_t i = 0; i < table.size(); ++i) {
+        const PCB &p = table[i];
+        cout << std::left << std::setw(6) << i
+             << std::setw(10) << (p.occupied ? 1 : 0);
+        if (p.occupied) {
+            cout << std::setw(12) << p.pid
+                 << std::setw(12) << p.start_sec
+                 << std::setw(12) << p.start_nano;
+        } else {
+            cout << std::setw(12) << "-" << std::setw(12) << "-" << std::setw(12) << "-";
+        }
+        cout << endl;
+    }
+    cout << endl;
+}
+
+void signal_handler(int sig) {
+    if (sig == SIGINT) {
+        cout << "Received SIGINT, terminating all child processes..." << endl;
+        // Terminate all child processes
+        kill(0, SIGTERM); // Send SIGTERM to all processes in the same process group
+        shmdt(shm_clock);
+        shmctl(shmid, IPC_RMID, nullptr);
+        exit(0);
     }
 }
 
@@ -119,24 +192,32 @@ int main(int argc, char* argv[]) {
     }
 
     // create shared memory
-    key_t sh_key = ftok("oss.cpp", 0);
-    int shmid = shmget(sh_key, sizeof(int)*2, IPC_CREAT | 0666);
+    sh_key = ftok("oss.cpp", 0);
+    shmid = shmget(sh_key, sizeof(int)*2, IPC_CREAT | 0666);
     if (shmid == -1) {
         perror("shmget");
         exit(1);
     }
 
     // attach shared memory to shm_ptr
-    int* clock = (int*) shmat(shmid, nullptr, 0);
-    if (clock == (int*) -1) {
+    shm_clock = (int*) shmat(shmid, nullptr, 0);
+    if (shm_clock == (int*) -1) {
         perror("shmat");
         exit(1);
     }
 
     // pointers to seconds and nanoseconds in shared memory
-    int *sec = &(clock[0]);
-    int *nano = &(clock[1]);
+    int *sec = &(shm_clock[0]);
+    int *nano = &(shm_clock[1]);
     *sec = *nano = 0;
+
+    // print interval using simulated clock: 0.5 seconds
+    const long long NSEC_PER_SEC = 1000000000LL;
+    const long long PRINT_INTERVAL_NANO = 500000000LL;
+    long long next_print_total = (long long)(*sec) * NSEC_PER_SEC + (long long)(*nano) + PRINT_INTERVAL_NANO;
+
+    signal(SIGALRM, signal_handler);
+    alarm(60);
 
     // oss staring message
     cout << "OSS starting, PID:" << getpid() << " PPID:" << getppid() << endl
@@ -156,12 +237,28 @@ int main(int argc, char* argv[]) {
         // checking if child has terminated
         pid_t term_pid = child_Terminated();
         if (term_pid > 0) {
+            remove_pcb(table, term_pid);
             running_processes--;
-            cout << "Child process terminated." << term_pid << endl;
+            cout << "Child process terminated: " << term_pid << endl;
+        }
+
+        // call print_process_table every half-second of simulated time
+        {
+            long long current_total = (long long)(*sec) * NSEC_PER_SEC + (long long)(*nano);
+            while (current_total >= next_print_total) {
+                print_process_table(table);
+                next_print_total += PRINT_INTERVAL_NANO;
+            }
         }
 
         if (launched_processes < proc && running_processes < simul) {
-            launch_worker(time_limit);
+            pid_t worker_pid = launch_worker(time_limit);
+            // find empty slot in PCB array and populate it with new process info
+            int pcb_index = find_empty_pcb(table);
+            table[pcb_index].occupied = true;
+            table[pcb_index].pid = worker_pid;
+            table[pcb_index].start_sec = *sec;
+            table[pcb_index].start_nano = *nano;
             launched_processes++;
             running_processes++;
         }
@@ -169,36 +266,7 @@ int main(int argc, char* argv[]) {
 
     cout << "Number of processes launched: " << launched_processes << endl;
 
-    shmdt(clock);
+    shmdt(shm_clock);
     shmctl(shmid, IPC_RMID, nullptr);
     return 0;
 }
-
-
-/*
-
-        if (launched_processes < proc) {
-            launched_processes++;
-            running_processes++;
-            pid_t worker_pid = fork();
-            if (worker_pid < 0) {
-                perror("fork failed");
-                exit(1);
-            } else
-
-            if (worker_pid == 0) {
-            // keep these std::string objects alive until execv runs
-                string arg_sec = to_string((int)time_limit);
-                string arg_nsec = to_string(seconds_conversion(time_limit));
-                char* args[] = {
-                    (char*)"./worker",
-                    const_cast<char*>(arg_sec.c_str()),
-                    const_cast<char*>(arg_nsec.c_str()),
-                    NULL
-                };
-            execv(args[0], args);
-            perror("Exec failed");
-            exit(1);
-            }
-        }
-*/
